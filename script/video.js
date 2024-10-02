36d36d69199134a29068553c513228c9f20903c8
Nahid Hasan <nahidn228@gmail.com>
2024-10-02 21:06:10 +0600

    time(hour minute second)added

--- script/video.js
+++ script/video.js
@@ -1,3 +1,12 @@
+function getTimeString(time) {
+  //get Hour and rest second
+  const hour = parseInt(time / 3600);
+  let remainSecond = time % 3600;
+  const minute = parseInt(remainSecond / 60);
+  const second = remainSecond % 60;
+  return `${hour} hour ${minute} minute ${remainSecond} second ago`;
+}
+
 const loadCategories = () => {
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then((res) => res.json())
@@ -41,17 +50,37 @@ const displayVideos = (videos) => {
     const card = document.createElement("div");
     card.classList = "card card-compact";
     card.innerHTML = `
-    <figure>
-    <img
+    <figure class="h-[200px] relative">
+    <img class = "h-full w-full object-cover"
       src="${video.thumbnail}"
       alt="Shoes" />
+      ${
+        video.others.posted_date?.length == 0
+          ? ""
+          : `<span class='absolute text-xs right-2 bottom-2 bg-black rounded p-1 text-gray-200'>
+      ${getTimeString(video.others.posted_date)}
+      </span>`
+      }
+      
   </figure>
-  <div class="card-body">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
+  <div class="px-0 py-2 flex gap-2">
+    
+    <div> <img class='w-10 h-10 rounded-full object-cover' src="${
+      video.authors[0].profile_picture
+    }" alt="" /> </div>
+    <div>
+    <h2 class='font-bold'>${video.title}</h2>
+    <div class='flex items-center gap-2'> 
+    <p class='text-gray-400'>${video.authors[0].profile_name}</p>
+    ${
+      video.authors[0].verified === true
+        ? `<img class='w-5' src="https://img.icons8.com/?size=100&id=D9RtvkuOe31p&format=png&color=000000" alt="" srcset="" />`
+        : ""
+    }
+    
+      </div>
+    <p></p>
+      </div>
   </div>
      
     `;
